ed7eb73a2b1a46b6ba81bc874c03c2a84dec35bb
Paul van Santen <pvansanten@gmail.com>
2019-07-18 10:48:49 +0200

    Refactor label logic into a separate class

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const yaml = require('js-yaml')
-const ignore = require('ignore')
-const normalizeForSearch = require('normalize-for-search')
+const Labeler = require("./lib/pr-labeler")
 
 /**
  * This is the main entrypoint to your Probot app
@@ -15,147 +13,13 @@ module.exports = app => {
   app.on('pull_request.synchronize', label)
 
   async function label(context) {
-    const pull = await context.github.pullRequests.get(context.issue())
+    // Create a PullRequestLabeler
+    const labeler = new Labeler(context, app.log)
 
-    app.log.info("Fetched updated pullrequest", pull.data.url)
+    // Fetch some PR info
+    await labeler.init()
 
-    if (pull.data.state !== "open") {
-      app.log.info("Pull request not open, ignoring", pull.data.state)
-      // We dont do anything with PRs that are closed/merged
-      return;
-    }
-
-    const content = await context.github.repos.getContents(context.repo({
-      path: '.github/pr-labeler.yml'
-    }))
-    const config = yaml.safeLoad(Buffer.from(content.data.content, 'base64').toString())
-
-    app.log.debug("Fetched config", config)
-
-    const files = await context.github.pullRequests.listFiles(context.issue())
-    const changedFiles = files.data.map(file => file.filename)
-
-    app.log.debug("Affected files", changedFiles)
-
-    const currentLabels = new Set(pull.data.labels.map((val) => {
-      return val.name
-    }))
-    const addLabels = new Set()
-    const allLabels = new Set()
-
-    app.log.debug("Current labels", currentLabels)
-
-    if ("file-path" in config) {
-      const filepaths = config["file-path"]
-
-      for (const label in filepaths) {
-        allLabels.add(label)
-
-        app.log.debug('Looking for file paths', label, filepaths[label])
-        const matcher = ignore().add(filepaths[label])
-
-        if (changedFiles.find(file => matcher.ignores(file))) {
-          app.log.info("Found file, applying label", label, filepaths[label])
-          addLabels.add(label)
-        }
-      }
-    }
-
-    if ("target-branch" in config) {
-      app.log.info("Pullrequest target branch is", pull.data.base.ref)
-
-      for (const label in config["target-branch"]) {
-        allLabels.add(label)
-
-        let branches = config["target-branch"][label]
-        // Allow non-array values:
-        if (!Array.isArray(branches)) {
-          branches = [branches]
-        }
-
-        for (const branch of branches) {
-          app.log.debug('Checking for target branch', label, branch)
-
-          if (pull.data.base.ref === branch) {
-            app.log.debug("Found branch", label, branch)
-            addLabels.add(label)
-          }
-        }
-      }
-    }
-
-    if ("author" in config) {
-      app.log.info("Pullrequest author is", pull.data.user.login)
-
-      for (const label in config["author"]) {
-        allLabels.add(label)
-
-        let authors = config["author"][label]
-        // Allow non-array values:
-        if (!Array.isArray(authors)) {
-          authors = [authors]
-        }
-
-        for (const author of authors) {
-          app.log.debug('Checking for author', label, author)
-
-          if (pull.data.user.login === author) {
-            app.log.debug("Found author, applying label", label, author)
-            addLabels.add(label)
-          }
-        }
-      }
-    }
-
-    if ("description" in config) {
-      for (const label in config["description"]) {
-        // We dont add labels in description to allLabels, because the softmatched
-        // labels should not be removed.
-
-        let keywords = config["description"][label]
-        // Allow non-array values:
-        if (!Array.isArray(keywords)) {
-          keywords = [keywords]
-        }
-
-        const normalizedBody = normalizeForSearch(pull.data.body)
-        for (const keyword of keywords) {
-          app.log.debug('Checking for keyword', label, keyword)
-
-          if (normalizedBody.indexOf(keyword) >= 0) {
-            app.log.debug("Found keyword, applying label", label, keyword)
-            addLabels.add(label)
-          }
-        }
-      }
-    }
-
-    const labelsToRemove = Array.from(allLabels).filter((value) => {
-      return !addLabels.has(value) && currentLabels.has(value)
-    })
-
-    const labelsToAdd = Array.from(addLabels).filter((value) => {
-      return !currentLabels.has(value)
-    })
-
-    app.log.info('Adding labels', labelsToAdd)
-    if (labelsToAdd.length > 0) {
-      context.github.issues.addLabels(context.issue({
-        labels: labelsToAdd
-      }))
-    }
-
-    if ("settings" in config && config["settings"]["remove-labels"]) {
-      app.log.info('Removing labels', labelsToRemove)
-      if (labelsToRemove.length > 0) {
-        for (const label of labelsToRemove) {
-          context.github.issues.removeLabel(context.issue({
-            name: label
-          }))
-        }
-      }
-    } else {
-      app.log.debug("Would have removed labels", labelsToRemove)
-    }
+    // Do the labeling!
+    await labeler.label()
   }
 }
